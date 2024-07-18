801dfcc47f279b06426421648d03f92d77477da0
d4ve101011 <125325797+d4ve101011@users.noreply.github.com>
2024-07-19 03:27:14 +0800

    hypr_up

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -621,13 +621,19 @@ export const supportsBalanceCheck = {
 
 export const visionModels = [
   'gpt-4o',
+  'gpt-4o-2024-05-13',
+  'gpt-4o-mini',
+  'gpt-4o-mini-2024-07-18',
   'gpt-4-turbo',
-  'gpt-4-vision',
-  'llava',
-  'llava-13b',
-  'gemini-pro-vision',
-  'claude-3',
-  'gemini-1.5',
+  'gpt-4-turbo-2024-04-09',
+  'gpt-4-vision-preview',
+  'gpt-4-1106-vision-preview',
+  'claude-3-haiku-20240307',
+  'claude-3-sonnet-20240229',
+  'claude-3-opus-20240229',
+  'claude-3-5-sonnet-20240620',
+  'gemini-1.5-pro',
+  'gemini-1.5-flash',
 ];
 export enum VisionModes {
   generative = 'generative',
