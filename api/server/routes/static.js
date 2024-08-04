f914a57c148e9684f619eb9d70b4a4863af3d324
d4ve101011 <125325797+d4ve101011@users.noreply.github.com>
2024-08-05 03:54:45 +0800

    del

--- api/server/routes/static.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const paths = require('~/config/paths');
-
-const router = express.Router();
-router.use(express.static(paths.imageOutput));
-
-module.exports = router;
